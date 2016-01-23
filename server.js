9a9f933df419b2471d3a0adfc481fa3e7c36ca23
Hsu Peng Jun <hsu.pengjun@icloud.com>
2016-01-23 23:30:33 +0800

    修改 server.js 檔案使其自動導向 index.html

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var http = require('http');
 var static_server = require('node-static');
 
 var port = normalizePort(process.env.PORT || '3000');
-var file_server = new static_server.Server('.');
+var file_server = new static_server.Server('.', {cache: 0});
 
 http.createServer(function (request, response) {
+    console.log(request.url);
     request.addListener('end', function () {
+      if (request.url.startsWith('/node_modules') || request.url.startsWith('/dist') || request.url.startsWith('/app')) {
         file_server.serve(request, response);
+      } else {
+        file_server.serveFile('/index.html', 200, {}, request, response);
+      }
     }).resume();
 }).listen(port);
 
